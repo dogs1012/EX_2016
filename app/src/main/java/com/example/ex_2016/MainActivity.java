8bcb61eafc60e676d8d9d7d3112a62b6d6544a85
USAN <dogs1012@naver.com>
2016-12-09 19:34:33 +0900

    프로젝트 생성, 레이아웃 생성 commit 작성 스위치 누를시 밑의 화면이 나오게 설계 일자설정 시간설정누르면 그에 맞는 뷰가나옴

--- app/src/main/java/com/example/ex_2016/MainActivity.java
+++ app/src/main/java/com/example/ex_2016/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.Chronometer;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -17,12 +18,14 @@ import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button BTN1, BTN2, BTN3,BTN4;
+    CalendarView cal;
     LinearLayout LL1, LL2;
     RadioGroup RG,RG2;
-    RadioButton RB1,RB2,RB3;
+    RadioButton RB1,RB2,RB3,RB4,RB5;
     ImageView IV;
     EditText ET1,ET2,ET3;
     FrameLayout FL;
@@ -32,7 +35,25 @@ public class MainActivity extends AppCompatActivity {
     TextView TV1,TV2,TV3;
     Chronometer CR;
     FrameLayout FM;
+    int num1,num2,num3, p1,p2,p3;
+    int hap =0;
+    int hap2;
 
+    void init()
+    {
+
+        this.ET1.setText("");
+        this.ET2.setText("");
+        this.ET3.setText("");
+        this.RB1.setChecked(true);
+        this.RB2.setChecked(true);
+        this.IV.setImageResource(R.drawable.s1);
+        this.TV1.setText("총 명수:");
+        this.TV2.setText("할인금액:");
+        this.TV3.setText("결제금액:");
+        this.BL1 = false;
+        this.BL2 = false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +68,19 @@ public class MainActivity extends AppCompatActivity {
         BTN3=(Button)findViewById(R.id.button7);
         LL1 =(LinearLayout)findViewById(R.id.LL1);
         LL2 =(LinearLayout)findViewById(R.id.LL2);
-        RG= (RadioGroup)findViewById(R.id.RG);
+
         RG2= (RadioGroup)findViewById(R.id.RG2);
         RB1=(RadioButton)findViewById(R.id.radioButton3);
         RB2=(RadioButton)findViewById(R.id.radioButton5);
         RB3=(RadioButton)findViewById(R.id.radioButton4);
+
         FM =(FrameLayout)findViewById(R.id.Frame);
+        ET1=(EditText) findViewById(R.id.editText);
+        ET2=(EditText) findViewById(R.id.editText2);
+        ET3=(EditText) findViewById(R.id.editText3);
+        TV1 =(TextView)findViewById(R.id.textView8);
+        TV2 =(TextView)findViewById(R.id.textView9);
+        TV3 =(TextView)findViewById(R.id.textView10);
         SH1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -72,11 +100,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        RG = (RadioGroup) findViewById(R.id.RG);
+
+        RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                     public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                              IV.setImageResource(R.drawable.s1);
+                               if (RB1.isChecked()) {
+                                      IV.setImageResource(R.drawable.s1);
+                                 } else if (RB2.isChecked()) {
+                                     IV.setImageResource(R.drawable.s2);
+                                 } else if (RB3.isChecked()) {
+                                    IV.setImageResource(R.drawable.s3);
+                                }
+                           }
+                });
+                BTN2 = (Button) findViewById(R.id.button4);
+
+
+                BTN2.setOnClickListener(new View.OnClickListener() {
+                     @Override
+                    public void onClick(View view) {
+                            LL1.setVisibility(View.INVISIBLE);
+                             LL2.setVisibility(View.VISIBLE);
+                         }
+                  });
+
+
+        TP1 =(TimePicker)findViewById(R.id.timePicker);
+             cal  = (CalendarView) findViewById(R.id.calendarView);
+            RB4 =(RadioButton)findViewById(R.id.radioButton11);
+         RB5 =(RadioButton)findViewById(R.id.radioButton10);
+        RB4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                                if (RB4.isChecked()) {
+                                     cal.setVisibility(View.VISIBLE);
+                                  TP1.setVisibility(View.INVISIBLE);
+                                    }
+                          }
+                    });
+               RB5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                     @Override
+                       public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            if (RB5.isChecked()) {
+                                     TP1.setVisibility(View.VISIBLE);
+                                    cal.setVisibility(View.INVISIBLE);
+                                   }
+                           }
+                   });
+
+
+
+
 
 
 
     }
-
 
 
 
